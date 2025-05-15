ca58914a8e5aa30f9f16c8b69503124fe899c64c
Aditya Banka <adityabanka1947@gmail.com>
2025-05-16 04:46:25 +0530

    unagi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,30 @@
 function Footer() {
   return (
-    <div className="h-32 bg-gray-800">
-      <p className="text-white text-center">This is a footer</p>
+    <div className="flex flex-row items-center justify-between px-20 min-h-32 bg-gray-800">
+      <div>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+      </div>
+      <div>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+      </div>
+      <div>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+      </div>
+      <div>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+        <p className="text-white text-center">This is a footer</p>
+      </div>
     </div>
   )
 }
