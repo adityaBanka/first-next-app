cf17db5ffc2da04b7b59bc4ef8442aacc7c07496
Aditya Banka <adityabanka1947@gmail.com>
2025-05-17 01:29:37 +0530

    wow I did soo mcuh (MUCH)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,61 @@
+import Link from 'next/link'
+import { Separator } from '@/components/ui/separator'
+import Image from "next/image"
+import Logo_text from "@/assets/logo_text.svg"
+
 function Footer() {
   return (
-    <div className="flex flex-row items-center justify-between px-20 min-h-32 bg-gray-800">
-      <div>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
+    <div className="flex flex-col items-center min-h-32 h-fit bg-gray-800">
+
+      <div className='flex items-start justify-center space-x-10 py-10'>
+
+        <div className='flex flex-col text-gray-300 space-y-1'>
+          <p className='text-lg text-white pb-1'>S Banka</p>
+          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Home</Link>
+          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Quiz</Link>
+          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Batches</Link>
+          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Terms and Conditions</Link>
+          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Privacy Policy</Link>
+        </div>
+
+        <Separator orientation="vertical" className='min-h-32 mx-10 my-auto' />
+
+        <div className='flex flex-col text-gray-300 space-y-1'>
+          <p className='text-lg text-white pb-1'>Contact</p>
+          <p>Primary: +91 - 9334200978</p>
+          <p>Alternate: +91 - 8294705101</p>
+          <p>Email: subhashbanka@gmail.com</p>
+        </div>
+
+        <Separator orientation="vertical" className='min-h-32 mx-10 my-auto' />
+
+        <div className='flex flex-col text-gray-300 space-y-1'>
+          <p className='text-lg text-white pb-1'>Address</p>
+          <p>Mr. Subhash Banka</p>
+          <p>401a & 412, 4th Floor</p>
+          <p>Verma Center, Boring Road Crossing</p>
+          <p>Patna, Bihar, India. 800-001</p>
+        </div>
       </div>
-      <div>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
+
+
+
+      <Separator className='mb-10 max-w-[90%]' />
+
+
+
+      <div className='mb-10 flex items-center justify-center space-x-10'>
+        
+        <Link href="/" className="inline-block pointer-events-auto">
+          <Image src={Logo_text} alt="logo" width={200} height={100} className="h-12" />
+        </Link>
+
+        <Separator orientation="vertical" className='min-h-12 h-full' />
+
+        <p className='text-gray-300'>© 2023 Subhash Banka. All rights reserved.</p>
+
       </div>
-      <div>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-      </div>
-      <div>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-        <p className="text-white text-center">This is a footer</p>
-      </div>
+
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
     const router = useRouter();
 
     return (
-        <div className="fixed w-full h-fit z-100 p-3 flex items-center justify-between">
+        <div className="fixed w-full h-fit z-100 p-3 flex items-center justify-between pointer-events-none">
 
             <ProgressiveBlur
                 className="h-32 w-full absolute top-0 left-0 -z-10"
@@ -19,11 +19,11 @@ function Header() {
                 direction="top"
             />
 
-            <Link href="/" className="inline-block">
+            <Link href="/" className="inline-block pointer-events-auto">
                 <Image src={Logo_text} alt="logo" width={200} height={100} className="h-12" />
             </Link>
 
-            <div className="flex items-center justify-center space-x-5">
+            <div className="flex items-center justify-center space-x-5 pointer-events-auto">
                 <button
                     onClick={() => router.push('/sign-in')}
                     className="p-3 bg-gray-300 rounded-xl hover:bg-gray-400"
