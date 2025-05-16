57417ae387747fd9874bcce3c44631f2b258770a
Aditya Banka <adityabanka1947@gmail.com>
2025-05-17 02:20:54 +0530

    kinna sona lag riya si

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,57 +5,64 @@ import Logo_text from "@/assets/logo_text.svg"
 
 function Footer() {
   return (
-    <div className="flex flex-col items-center min-h-32 h-fit bg-gray-800">
+    <div className="min-h-32 h-fit bg-gray-800">
 
-      <div className='flex items-start justify-center space-x-10 py-10'>
+      <div className='flex justify-center px-10 py-10'>
 
-        <div className='flex flex-col text-gray-300 space-y-1'>
-          <p className='text-lg text-white pb-1'>S Banka</p>
-          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Home</Link>
-          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Quiz</Link>
-          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Batches</Link>
-          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Terms and Conditions</Link>
-          <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Privacy Policy</Link>
+        <div className='flex items-start justify-center'>
+
+          <div className='flex flex-col text-gray-300 space-y-1'>
+            <p className='text-lg text-white pb-1 underline underline-offset-2'>S Banka</p>
+            <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Home</Link>
+            <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Quiz</Link>
+            <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Batches</Link>
+            <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Terms and Conditions</Link>
+            <Link href="/" className="hover:text-white hover:underline underline-offset-2 w-fit">Privacy Policy</Link>
+          </div>
+
+          <Separator orientation="vertical" className='min-h-32 my-auto mx-10' />
+
+          <div className='flex flex-col text-gray-300 space-y-1'>
+            <p className='text-lg text-white pb-1 underline underline-offset-2'>Address</p>
+            <p>Mr. Subhash Banka</p>
+            <p>401a & 412, 4th Floor</p>
+            <p>Verma Center, Boring Road Crossing</p>
+            <p>Patna, Bihar, India</p>
+            <p>Postal Code - 800001</p>
+
+            <p className='text-lg text-white pb-1 mt-2 underline underline-offset-2'>Contact</p>
+            <p>Phone Number: +91 - 9334200978</p>
+            {/* <p>Alternate: +91 - 8294705101</p>
+          <p>Email: subhashbanka@gmail.com</p> */}
+          </div>
+
+          <Separator orientation="vertical" className='min-h-32 my-auto mx-10' />
+
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2019.1750522992584!2d85.11758207045936!3d25.612863300941545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed583b6b51a497%3A0xb85436170f2a7326!2sBanka%20Computer%20Education!5e0!3m2!1sen!2sin!4v1747428225007!5m2!1sen!2sin" width="500" height="300" loading="lazy"></iframe>
+
         </div>
 
-        <Separator orientation="vertical" className='min-h-32 mx-10 my-auto' />
 
-        <div className='flex flex-col text-gray-300 space-y-1'>
-          <p className='text-lg text-white pb-1'>Contact</p>
-          <p>Primary: +91 - 9334200978</p>
-          <p>Alternate: +91 - 8294705101</p>
-          <p>Email: subhashbanka@gmail.com</p>
-        </div>
 
-        <Separator orientation="vertical" className='min-h-32 mx-10 my-auto' />
-
-        <div className='flex flex-col text-gray-300 space-y-1'>
-          <p className='text-lg text-white pb-1'>Address</p>
-          <p>Mr. Subhash Banka</p>
-          <p>401a & 412, 4th Floor</p>
-          <p>Verma Center, Boring Road Crossing</p>
-          <p>Patna, Bihar, India. 800-001</p>
-        </div>
       </div>
 
 
+      <Separator className='max-w-[80%] mx-auto mb-10' />
 
-      <Separator className='mb-10 max-w-[90%]' />
+      <div className='pb-10 flex items-center justify-center space-x-10 h-full'>
 
-
-
-      <div className='mb-10 flex items-center justify-center space-x-10'>
-        
         <Link href="/" className="inline-block pointer-events-auto">
           <Image src={Logo_text} alt="logo" width={200} height={100} className="h-12" />
         </Link>
 
         <Separator orientation="vertical" className='min-h-12 h-full' />
 
-        <p className='text-gray-300'>© 2023 Subhash Banka. All rights reserved.</p>
+        <div className='text-gray-300'>
+          <p>© 2025 sbanka.in & it&apos;s properties. All rights reserved.</p>
+          <Link href="https://www.adityabanka.com/" className='hover:underline hover:text-white underline-offset-1'>Developed and maintained by Aditya Banka.</Link>
+        </div>
 
       </div>
-
     </div>
   )
 }
